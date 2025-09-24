c815618d9fe4fbcac6fc0602a0ca707e99bf8e07
yumauno-code <233423550yumauno-code@users.noreply.github.com>
2025-09-25 00:28:54 +0900

    Add Kuku program

---
+++ Kuku.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Kuku {
+    public static void main(String[] args) {
+        for(int i=1;i<10;i++){
+            for(int j=1; j<10; j++){
+                System.out.printf("%3d",i*j);
+            }
+            System.out.print("\n");
+        }
+    }
+}

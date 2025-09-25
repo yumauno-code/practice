ff35e138a3326b4498e1257ce84e9444647341ac
yumauno-code <233423550yumauno-code@users.noreply.github.com>
2025-09-25 17:13:29 +0900

    Update Kuku add message

--- Kuku.java
+++ Kuku.java
@@ -2,6 +2,7 @@ package practice;
 
 public class Kuku {
     public static void main(String[] args) {
+        System.out.println("九九の表示");
         for(int i=1;i<10;i++){
             for(int j=1; j<10; j++){
                 System.out.printf("%3d",i*j);

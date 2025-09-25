96be693bf70add81f0805a1619e00ae417d5a256
yumauno-code <233423550yumauno-code@users.noreply.github.com>
2025-09-25 17:10:16 +0900

    Update HelloWorld こんにちは!

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ package practice;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld, A and B!");
+        System.out.println("こんにちは！");
     }
 }

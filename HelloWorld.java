5f2df08f208b1ea81a1402727d0a1e3816dde826
yumauno-code <233423550yumauno-code@users.noreply.github.com>
2025-09-25 15:30:56 +0900

    Change message in branch A

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package practice;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld,feature!!");
+        System.out.println("HelloWorld from A!");
     }
 }
